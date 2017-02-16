b626672a1a3f8cf62ad43bfd7958434ba1778373
Nathan <nharrisanalyst@gmail.com>
2017-02-16 12:32:53 -0800

    created html shell and made ajax call for data

---
+++ salaryGraph.js
@@ -0,0 +1,20 @@
+//d3 boilerplate
+
+
+
+//data and making the chart
+d3.csv('data/salaryData.csv',function(err,data){
+    if(err){console.log('there was an error')}
+     else{
+     
+     console.log(data)
+     
+     //chart goes here
+     
+     
+     
+     
+     }
+     
+     
+});
